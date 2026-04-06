9335cc205f7f0b938727e0a1bf595b0d62f5d1ae
joelyakubu269 <joelyakubu269@gmail.com>
2026-04-06 04:56:17 +0100

    Here I print shapes using nested for loops

--- shapes.go
+++ shapes.go
@@ -8,21 +8,21 @@ func main() {
 	gRows(25)
 }
 
-//	func gRows(rows int) {
-//		for i := 1; i <= rows; i++ {
-//			for j := 1; j <= i; j++ {
-//				fmt.Print("G")
-//			}
-//			fmt.Println()
-//		}
-//	}
-//
-// An alternative method
 func gRows(rows int) {
-	for i := 0; i < rows; i++ {
-		for j := 0; j < i+1; j++ {
+	for i := 1; i <= rows; i++ {
+		for j := 1; j <= i; j++ {
 			fmt.Print("G")
 		}
 		fmt.Println()
 	}
 }
+
+// An alternative method
+// func gRows(rows int) {
+// 	for i := 0; i < rows; i++ {
+// 		for j := 0; j < i+1; j++ {
+// 			fmt.Print("G")
+// 		}
+// 		fmt.Println()
+// 	}
+// }
