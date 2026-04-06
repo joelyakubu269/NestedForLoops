f882380facf7070026cda90e9a9aa092973871bd
joelyakubu269 <joelyakubu269@gmail.com>
2026-04-06 12:27:03 +0100

    Here I print a pyramid from the right  using an extra loop in the outer

--- pyramidLeft.go
+++ pyramidLeft.go
@@ -7,7 +7,7 @@ func main() {
 }
 func pyramids(rows int) {
 	for i := 1; i <= rows; i++ {
-		for k := 0; k < rows-i; k++ {
+		for k := 0; k < rows-i; k++ { // this extra loop is used to control when you print from a position other than the left
 			fmt.Print(" ")
 		}
 		for j := 0; j < i; j++ {
